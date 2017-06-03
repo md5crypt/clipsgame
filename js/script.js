68239ba33f93d6eacc03a7f343bb670240c8435c
md5crypt <borsuczek@gmail.com>
2017-06-04 00:28:52 +0200

    'lil fix

--- js/script.js
+++ js/script.js
@@ -178,7 +178,7 @@ $(function(){
 	}
 	function colorSyntax(code){
 		var last = 'other';
-		return code.replace(/(\?[^\s()]*)|\b(defrule|deffacts|assert|retract|not|and|or|forall|exists)\b|([=><()-]+)|([^\s()]+)/g,function(match,variable,keyword,operator){
+		return code.replace(/(\?[^\s()]*)|\b(defrule|deffacts|assert|retract|not|and|or|forall|exists)\b|((?:[=><()-]+|&#62;|&#60;|&gt;|&lt;))|([^\s()]+)/g,function(match,variable,keyword,operator){
 			var type = 'other';
 			if(variable)
 				type = 'variable';
